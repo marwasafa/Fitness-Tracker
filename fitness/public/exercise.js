bf4a8f32eb780b0377a9d157ec93599c596fbf04
marwasafa <32312941+marwasafa@users.noreply.github.com>
2021-03-01 20:36:56 -0500

    Update exercise.js

--- fitness/public/exercise.js
+++ fitness/public/exercise.js
@@ -19,64 +19,13 @@ import { useForm } from "react-hook-form";
 
 const onSubmit = (data, r) => {
   alert(`Thank you for your message from ${data.email}` );
-  const templateId = 'template_twqap0r';
-  const serviceID = 'service_r0tmsro';
+  const templateId = 'template_l7s9qxd';
+  const serviceID = 'my_gmail';
   sendFeedback(serviceID, templateId, { from_name: data.name, message_html: data.comment, reply_to: data.email })
   r.target.reset();
 }
 
 
-  return (
-      <div className="ContactForm">
-          <form onSubmit={handleSubmit(onSubmit)}>
-              <input 
-                  placeholder="name"
-                  name="name" 
-                  ref={
-                      register({ 
-                          required: "Please enter your name", 
-                          maxLength: {
-                              value: 20,
-                              message: "Please enter a name with fewer than 20 characters"
-                          } 
-                      })
-                  } 
-              /><br />
-              {errors.name && errors.name.message}<br />
-
-              <input
-                  placeholder="email"
-                  name="email"
-                  ref={
-                      register({
-                          required: "Please enter an email",
-                          pattern: {
-                              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                              message: "invalid email address"
-                          }
-                      })
-                  }
-              /><br/>
-              {errors.email && errors.email.message}<br />
-
-              <textarea 
-                  placeholder="comment"
-                  name="comment" 
-                  ref={
-                      register({
-                          required: true
-                      })
-                  } 
-              /><br />
-              {errors.comment && "oops, you forgot your message!"}<br />
-
-              <input type="submit" />
-          </form>
-      </div>
-  );
-}
-
-export default Contact;
 
 
 
